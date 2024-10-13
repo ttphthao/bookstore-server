ecd7cbb2ee6edfc5a1b8042de742f007b7eff37a
Thao Tran <ttphuongthao57@gmail.com>
2024-10-14 01:15:43 +0700

    [Chore]: soft fix

--- router/account/need_login_account.router.js
+++ router/account/need_login_account.router.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const AccountController = require('../../controller/account.controller');
 
-router.put('/changePassword', AccountController.changePassword);
+router.put('/change_password', AccountController.changePassword);
 router.get('/info', AccountController.info);
 router.post('/signout', AccountController.signout);
 
